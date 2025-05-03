f15953927691009dc19c2a5f4a7bd1967badc96d
tejasteju12 <steja.1si21ad059@gmail.com>
2025-05-03 12:43:15 +0530

    feat: update dependencies and add new packages for authentication and date handling
    refactor: remove old migration file and create new migration with updated schema
    
    chore: update prisma schema to include new models and relationships for user accounts and sessions
    
    fix: enhance environment configuration with additional required variables for better authentication

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,15 @@
 // Example environment configuration
 export const jwtSecretKey = process.env.JWT_SECRET_KEY || process.exit(1);
-
-export const env = {
+const env = {
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: process.env.PORT || 3000,
   };
+  
+
+export const databaseUrl = process.env.DATABASE_URL || process.exit(1);
+export const directUrl = process.env.DIRECT_URL || process.exit(1);
+
+export const serverUrl = process.env.SERVER_URL || process.exit(1);
+export const webClientUrl = process.env.WEB_CLIENT_URL || process.exit(1);
+export const betterAuthSecret = process.env.BETTER_AUTH_SECRET || process.exit(1);
+export { env };
